41b2e080850561c5963bf06e8ae9cb50f141e25a
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-09-27 13:10:04 +0530

    modules

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "packer" {
-    source = "./packer-module"
+    source = "./modules/packer-module"
 }
 
 module "create-instance" {
-  source = "./create-instance-module"
+  source = "./modules/create-instance-module"
   ami_id = trimspace(split(":", module.packer.image_name)[1]) 
 }
 
 module "docker" {
-  source = "./docker-module"
+  source = "./modules/docker-module"
   instance_pulic_ip = module.create-instance.instance_public_ip
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "instance_public_ip" {
-  value = module.create-instance.instance_public_ipd
+  value = "${module.create-instance.instance_public_ip}"
 }
