b5ab27a7de8580986009c6b8da008ee43393cd8e
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-09-29 00:22:37 +0530

    changed dir name

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ module "packer" {
     source = "./modules/packer-module"
 }
 
-module "create-instance" {
-  source = "./modules/create-instance-module"
+module "instance" {
+  source = "./modules/instance-module"
   ami_id = trimspace(split(":", module.packer.image_name)[1]) 
 }
 
 module "docker" {
   source = "./modules/docker-module"
-  instance_pulic_ip = module.create-instance.instance_public_ip
+  instance_pulic_ip = module.instance.instance_public_ip
 }
