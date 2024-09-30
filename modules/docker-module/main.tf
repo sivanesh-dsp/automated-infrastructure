f9055e60e3545b70f232863196ef21e6155436b5
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-09-30 17:28:13 +0530

    docker-module

--- modules/docker-module/main.tf
+++ modules/docker-module/main.tf
@@ -7,10 +7,73 @@ terraform {
     }
 }
 
-provider "docker" {
-  host = "tcp://${ var.instance_pulic_ip }:2375"  
+  provider "docker" {
+    host = "tcp://${ var.instance_public_ip }:2375"  
+  }
+
+
+# Define Docker volumes
+# iterative
+resource "docker_volume" "sonarqube_volumes" {
+  for_each = var.sonarqube_volumes
+    name = each.key
 }
 
-resource "docker_image" "jenkins" {
-  name="sonarqube:alpine"
+# Define Docker container
+resource "docker_container" "sonarqube" {
+  depends_on = [ docker_volume.sonarqube_volumes ]
+  image = var.sonarqube_image
+  name  = var.sonarqube_container_name
+  ports {
+    internal = 9000
+    external = var.sonarqube_ports["9000"]
+  }
+  dynamic "volumes" {
+    for_each = var.sonarqube_volumes
+    content {
+      volume_name    = volumes.key
+      container_path = volumes.value
+    }
+  }
+}
+
+# resource "null_resource" "creating_user_in_jenkins" {
+#   depends_on = [ docker_container.sonarqube ]
+#   provisioner "local-exec" {
+#     environment = {
+#       ANSIBLE_HOST_KEY_CHECKING = "False"
+#     }
+#     command = <<EOT
+#       sudo ansible-playbook -i ${var.instance_public_ip}, \
+#       --private-key ~/.ssh/${var.instance_type}.pem \
+#       -e ansible_user=ubuntu \
+#       ~/poc4/modules/docker-module/sonarqube-user.yml
+#     EOT
+#   }
+# }
+
+# Create Ansible inventory file for Jenkins and SonarQube
+resource "local_file" "inventory_ini" {
+  content = <<EOT
+[Sonarqube]
+sonarqube ansible_host=${var.instance_public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/${var.instance_key}.pem
+
+EOT
+  filename = "${path.module}/playbook/inventory.ini"
+}
+
+# Run Ansible Playbook after generating the inventory file
+resource "null_resource" "run_sonarqube_playbooks" {
+  depends_on = [local_file.inventory_ini]
+  
+  provisioner "local-exec" {
+        environment = {
+      ANSIBLE_HOST_KEY_CHECKING = "False"
+    }
+
+    command = <<-EOT
+      ansible-playbook -i ${path.module}/playbook/inventory.ini ${path.module}/playbook/sonarqube-user.yml \
+      -e "ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+    EOT
+  }
 }
--- modules/docker-module/variables.tf
+++ modules/docker-module/variables.tf
@@ -1,4 +1,38 @@
-variable "instance_pulic_ip" {
+variable "instance_public_ip" {
   description = "The public ip to use for the EC2 instance"
   type        = string
+}
+
+variable "sonarqube_image" {
+  description = "The Docker image for SonarQube."
+  type        = string
+  default     = "sonarqube:10.6-community"
+}
+
+variable "sonarqube_container_name" {
+  description = "The name of the SonarQube container."
+  type        = string
+  default     = "sonarqube"
+}
+
+variable "sonarqube_ports" {
+  description = "Ports to expose for the SonarQube container."
+  type        = map(number)
+  default     = {
+    "9000" = 9000
+  }
+}
+
+variable "sonarqube_volumes" {
+  description = "Mapping of volume names to container paths for SonarQube."
+  type        = map(string)
+  default     = {
+    "sonarqube_data"      = "/opt/sonarqube/data"
+    "sonarqube_logs"      = "/opt/sonarqube/logs"
+    "sonarqube_extensions" = "/opt/sonarqube/extensions"
+  }
+}
+
+variable "instance_key" {
+  description = "Key for instance"
 }
