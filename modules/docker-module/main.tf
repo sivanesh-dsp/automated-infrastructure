6e584c65bb1cceb40b930152b3ab8972e61b2003
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-10-03 12:16:58 +0530

    docker without sudo

--- modules/docker-module/main.tf
+++ modules/docker-module/main.tf
@@ -11,7 +11,18 @@ terraform {
     host = "tcp://${ var.instance_public_ip }:2375"  
   }
 
+resource "null_resource" "docker-without-sudo" {
+    provisioner "local-exec" {
+        environment = {
+      ANSIBLE_HOST_KEY_CHECKING = "False"
+    }
 
+    command = <<-EOT
+      ansible-playbook -i ${path.module}/playbook/inventory.ini ${path.module}/docker.yml \
+      -e "ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+    EOT
+  }
+}
 # Define Docker volumes
 # iterative
 resource "docker_volume" "sonarqube_volumes" {
