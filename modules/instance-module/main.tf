00bec850b91764014d066691b397d720019efb2b
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-10-03 16:42:40 +0530

    volume size

--- modules/instance-module/main.tf
+++ modules/instance-module/main.tf
@@ -7,6 +7,12 @@ resource "aws_instance" "custom_ami_instance" {
   instance_type = var.instance_type
   key_name = var.instance_key
   security_groups = [ "server-sg" ]
+
+  root_block_device {
+    volume_size = 15  # Size in GB
+    volume_type = "gp2"  # General Purpose SSD (gp2) is the default type
+  }
+
   tags = {
     Name = "Custom-AMI-Docker-Instance"
   }
@@ -23,7 +29,7 @@ resource "null_resource" "creating_user_in_jenkins" {
       sleep 10;
       ansible-playbook -i ${aws_instance.custom_ami_instance.public_ip}, \
       --private-key ~/.ssh/${var.instance_key}.pem \
-      ~/poc4/modules/instance-module/jenkins-user.yml
+      ${path.module}/jenkins-user.yml
     EOT
   }
 }
