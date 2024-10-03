3ecbf9df3de3e9e3722015532482102b81a29fbd
sivanesh-dsp <sivaneshsiva240@gmail.com>
2024-10-03 16:44:27 +0530

    clean

--- modules/ansible-module/main.tf
+++ modules/ansible-module/main.tf
@@ -9,7 +9,7 @@ resource "local_file" "inventory_ini" {
 
 resource "null_resource" "sonarqube" {
   provisioner "local-exec" {
-        environment = {
+    environment = {
       ANSIBLE_HOST_KEY_CHECKING = "False"
     }
 
@@ -20,24 +20,8 @@ resource "null_resource" "sonarqube" {
   }
 }
 
-resource "null_resource" "Jenkins" {
-  depends_on = [null_resource.sonarqube]
-  
-  provisioner "local-exec" {
-        environment = {
-      ANSIBLE_HOST_KEY_CHECKING = "False"
-    }
-
-    command = <<-EOT
-      ansible-playbook -i ${path.module}/playbook/inventory.ini ${path.module}/playbook/jenkins.yml \
-      -e "ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
-    EOT
-  }
-}
-
 
 resource "null_resource" "Jenkinsjob" {
-  depends_on = [ null_resource.Jenkins ]  
   provisioner "local-exec" {
         environment = {
       ANSIBLE_HOST_KEY_CHECKING = "False"
